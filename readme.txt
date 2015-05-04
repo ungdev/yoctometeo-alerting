2552e4e4ff7d091686a2fe6c1b7b851d15291baf
juliencanat <julien.canat@utt.fr>
2015-05-04 16:58:08 +0200

    functionnal sms and mail alert
    warning will alert every 1 minute

---
+++ readme.txt
@@ -0,0 +1,14 @@
+Read me before changing conf file!!
+Here is how to to write a good conf file for yoctometeo-alerting python script created by bijou
+
+First get the serial name(YOCTOMETEOMK******) (or logical name) of all your modules, put it in the dict list "module" along with
+their hosts (IP of corresponding virtual hub (if local: 127.0.0.1 or localhost))
+
+Then in the "mesures" dict list specify unit !! only C(for celsius degre),mbar and %rh are handled at the moment !!
+The resolution is not yet handled (coming soon)
+Threshold is important only two means of communication are handled at the moment: mail and sms via free-mobile api, specify the level corresponding
+to the type of alert (warning, error or critical) for logs, don't forget to specify the value of the threshold.
+At the end fill the users dict list, user is just there to be used by you to find who is who in the config file. If the user doesn't have a
+free-mobile account then just put a blank string "". sms won't be sent, a mail will replace it.
+
+At the moment each time a threshold is exceeded corresping alert is sent. (working on it)
--- yocto.py
+++ yocto.py
@@ -1,94 +1,143 @@
-import json, yocto_api, yocto_humidity, yocto_temperature, yocto_pressure, logging, sys
+import json
+import yocto_api
+import yocto_humidity
+import yocto_temperature
+import yocto_pressure
+import logging
+import sys
+import smtplib
+import urllib.request
+from email.mime.text import MIMEText
 
-modules =[]
+me = "yoctometeo@utt.fr"
+modules = []
 captor_types = []
+users_list = []
 
 
 def die(msg):
-    sys.exit(msg+' (check USB cable)')
+    sys.exit(msg + '(check USB cable)')
+
 
 class Mesure(object):
     """A class for mesures"""
-    def __init__(self, unit, captor=None, threshold=None, value=None,Captor=None, Module = None, host=None):
+
+    def __init__(self, unit, module, captor=None, threshold=None, host=None, logical_name = None):
         self.captor = captor
         self.unit = unit
-        self.value = value
+        self.value = None
         self.threshold = threshold
-        self.Captor = Captor
-        self.Module = Module
-        self.Real_module = None
+        self.physical_captor = None
+        self.module = module
+        self.physical_module = None
         self.Host = host
-        #creating loggers for the mesure using logging library
-        s = " " + self.unit
+        self.type = ""
+        self.logical_name = logical_name
+        # creating loggers for the mesure using logging library
+        s = self.unit
         self.logger = logging.getLogger(s)
-        self.logger.setLevel(logging.DEBUG)
+        self.logger.setLevel(logging.INFO)
         self.ch = logging.StreamHandler()
-        self.ch.setLevel(logging.DEBUG)
+        self.ch.setLevel(logging.INFO)
         self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         self.ch.setFormatter(self.formatter)
         self.logger.addHandler(self.ch)
         #Syslog usage
         #self.sh = logging.handlers.SysLogHandler(address = ('localhost',514), facility=19)
-        #self.sh.setLevel(logging.DEBUG)
+        #self.sh.setLevel(logging.INFO)
         #self.sh.setFormatter(self.formatter)
         #self.logger.addHandler(self.sh)
 
     def get_module(self):
-        self.Real_module = yocto_api.YModule.get_module(self.Captor)
+        self.physical_module = yocto_api.YModule.get_module(self.physical_captor)
 
     """attribute module to mesure object"""
+
     def get_captor(self):
-        #determine which type of captor (=> API) to use and associate the module
+        # determine which type of captor (=> API) to use and associate the module
         if self.unit == "%rh":
-            errmsg =  yocto_api.YRefParam()
-            if yocto_api.YAPI.RegisterHub(self.Host,errmsg)  != yocto_api.YAPI.SUCCESS:
-                 sys.exit("init error"+errmsg.value)
+            errmsg = yocto_api.YRefParam()
+            if yocto_api.YAPI.SUCCESS != yocto_api.YAPI.RegisterHub(self.Host, errmsg):
+                sys.exit("init error" + errmsg.value)
 
-            self.Captor = yocto_humidity.YHumidity.FindHumidity(self.Module+".humidity")
+            self.physical_captor = yocto_humidity.YHumidity.FindHumidity(self.module + ".humidity")
+            self.type = "humidity"
 
         elif self.unit == "mbar":
-            errmsg =  yocto_api.YRefParam()
-            if yocto_api.YAPI.RegisterHub(self.Host,errmsg)  != yocto_api.YAPI.SUCCESS:
-                 sys.exit("init error"+errmsg.value)
+            errmsg = yocto_api.YRefParam()
+            if yocto_api.YAPI.SUCCESS != yocto_api.YAPI.RegisterHub(self.Host, errmsg):
+                sys.exit("init error" + errmsg.value)
 
-            self.Captor = yocto_pressure.YPressure.FindPressure(self.Module+".pressure")
+            self.physical_captor = yocto_pressure.YPressure.FindPressure(self.module + ".pressure")
+            self.type = "pressure"
 
         elif self.unit == "C":
-            errmsg =  yocto_api.YRefParam()
-            if yocto_api.YAPI.RegisterHub(self.Host,errmsg)  != yocto_api.YAPI.SUCCESS:
-                 sys.exit("init error"+errmsg.value)
+            errmsg = yocto_api.YRefParam()
+            if yocto_api.YAPI.SUCCESS != yocto_api.YAPI.RegisterHub(self.Host, errmsg):
+                sys.exit("init error" + errmsg.value)
 
-            self.Captor = yocto_temperature.YTemperature.FindTemperature(self.Module+".temperature")
+            self.physical_captor = yocto_temperature.YTemperature.FindTemperature(self.module + ".temperature")
+            self.type = "temperature"
 
-        else :
-            self.logger.critical("units!!!!!")
-
-    def get_value(self):
-        if self.unit == "%rh":
-            self.value = self.Captor.get_currentValue()
-        elif self.unit == "mbar":
-            self.value = self.Captor.get_currentValue()
-        elif self.unit == "C":
-            self.value = self.Captor.get_currentValue()
         else:
             self.logger.critical("units!!!!!")
 
-    """sends mail to people"""
-  #  def send_mail(self, user):
+    def get_value(self):
+        self.value = self.physical_captor.get_currentValue()
 
-#class User(object):
-#    def __init__(self, name, mail, number):
-#        self.name = name
-#        self.mail = mail
-#        self.number = number
+    def log_sms(self):
+        if self.threshold["sms"]["level"] == "warning":
+            self.logger.warning("")
+        elif self.threshold["sms"]["level"] == "error":
+            self.logger.error("")
+        elif self.threshold["sms"]["level"] == "critical":
+            self.logger.critical("")
+        else:
+            self.logger.critical("")
 
-#real code now
+    def log_mail(self):
+        if self.threshold["mail"]["level"] == "warning":
+            self.logger.warning("")
+        elif self.threshold["mail"]["level"] == "error":
+            self.logger.error("")
+        elif self.threshold["mail"]["level"] == "critical":
+            self.logger.critical("")
+        else:
+            self.logger.critical("")
+
+
+class User(object):
+    def __init__(self, name, mail, free_url):
+        self.name = name
+        self.mail = mail
+        self.free_url = free_url
+
+    def send_mail(self, subject, text):
+        msg = MIMEText(text)
+        msg['Subject'] = subject
+        msg['From'] = me
+        msg['To'] = self.mail
+        s = smtplib.SMTP('smtp.utt.fr')
+        s.sendmail(me, self.mail, msg.as_string())
+        s.quit()
+
+    def send_sms(self, text):
+        if self.free_url != "":
+            urllib.request.urlopen(self.free_url + text)
+        else:
+            self.send_mail("sms alert",text)
+
+# real code now
 #loading json config
 with open('config.json') as data_file:
     data = json.load(data_file)
     for module in data["configuration"]["module"]:
         for element in data["configuration"]["mesures"]:
-            captor_types.append(Mesure(element["unit"], threshold=element["threshold"],host=data["configuration"]["host"],Module=module))
+            captor_types.append(
+                Mesure(element["unit"], module["name"], threshold=element["threshold"], host=module["host"],
+                       logical_name=module["logical"]))
+    for guy in data["configuration"]["users"]:
+        users_list.append(User(guy["user"], guy["mail"], guy["Free_url"]))
 
 for mesure in captor_types:
     mesure.get_captor()
@@ -98,15 +147,22 @@ for mesure in captor_types:
 while True:
     for mesure in captor_types:
         mesure.get_value()
-        mesure.logger.debug(str(mesure.value))
-        if not mesure.Real_module.isOnline():
-            mesure.logger.critical("je suis déconnecté, faites quelque chose!!")
-            mesure.alert_mail("déconnexion")
-            mesure.alert_sms("déconnexion")
-        if mesure.value >= mesure.threshold["mail"][1]:
-            # appeler la bonne méthode
-            #    mesure.alert_+trigger()
-            mesure.logger.warning("attention")
-        if mesure.value >= mesure.threshold["sms"][1]:
-            mesure.logger.critical("Wowowowowow")
+        mesure.logger.info(str(mesure.value))
+        if not mesure.physical_module.isOnline():
+            mesure.logger.critical("deconnected %s, do something!!" % mesure.logical_name)
+            for user in users_list:
+                user.send_mail("YoctoMeteo alert", "deconnected %s!!" % mesure.logical_name)
+                user.send_sms("YoctoMeteo deconnected %s" % mesure.logical_name)
+
+        if mesure.value >= mesure.threshold["mail"]["value"]:
+            mesure.log_mail()
+            for user in users_list:
+                user.send_mail("YoctoMeteo alert", "%s %s is at %s %s"
+                               % (mesure.logical_name, mesure.type, str(mesure.value), mesure.unit))
+
+        if mesure.value >= mesure.threshold["sms"]["value"]:
+            mesure.log_sms()
+            for user in users_list:
+                user.send_sms("%s %s is at %s %s" % (mesure.logical_name, mesure.type, str(mesure.value), mesure.unit))
+
     yocto_api.YAPI.Sleep(60000)
