3ad7429b1bf9edd3fea05d6a341971be0be66281
Adrien J-M. C. Rabian <adrien@rabian.fr>
2016-04-05 16:45:37 +0200

    Adding support directionned triggers.

--- alert.py
+++ alert.py
@@ -2,16 +2,18 @@
 
 class Alert(object):
 
-    def __init__(self, sensor, alert_vector=None, level=None, trigger=None, reset=None, json_config=None):
+    def __init__(self, sensor, alert_vector=None, level=None, direction=None, trigger=None, reset=None, json_config=None):
         if json_config is not None:
             self.vector = json_config["alert-vector"]
             self.level = json_config["level"]
+            self.direction = json_config["direction"]
             self.trigger = json_config["trigger"]
             self.reset = json_config["reset"]
             self.sensor = sensor
         else:
             self.vector = alert_vector
             self.level = level
+            self.direction = direction
             self.trigger = trigger
             self.reset = reset
             self.sensor = sensor
