fb88fac7da66d45740e06cb7d6d0146c268afb68
Adrien J-M. C. Rabian <adrien@rabian.fr>
2016-04-05 16:19:11 +0200

    Main : dump values to console (debug).

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
+import time
 from module import Module
 from sensor import Sensor
 from alert import Alert
+from yoctopuce.yocto_api import *
 
 modules = []
 sensors = []
@@ -12,11 +14,18 @@ with open('config.json') as config_file:
     mail_config = config["mail-server"]
     for module in config["modules"]:
         obj_module = Module(json_config=module)
+        obj_module.get_hw_module()
         modules.append(obj_module)
         for sensor in module["sensors"]:
             obj_sensor = Sensor(obj_module, json_config=sensor)
+            obj_sensor.get_hw_sensor()
             sensors.append(obj_sensor)
             for alert in sensor["alerts"]:
                 obj_alert = Alert(obj_sensor, alert)
                 alerts.append(obj_alert)
 
+while True:
+    print("---- %s ----" % time.asctime(time.localtime(time.time())))
+    for sensor in sensors:
+        print("Module %s, %s sensor : %f %s" % (sensor.module.hwid, sensor.type, sensor.get_value(), sensor.get_unit()))
+    YAPI.Sleep(1000)
