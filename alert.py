78ccf6b3b4abc0b2f2f42eb0a36ba50697adbecd
Adrien J-M. C. Rabian <adrien@rabian.fr>
2016-04-07 22:06:22 +0200

    Alert inheritance.

--- alert.py
+++ alert.py
@@ -1,22 +1,47 @@
 from alerting import *
 
+
 class Alert(object):
 
-    def __init__(self, sensor, alert_vector=None, level=None, direction=None, trigger=None, reset=None, status="iddle", json_config=None):
+    def __init__(self, alert_vector=None, level=None, status="iddle", alert_type="generic"):
+        self.vector = alert_vector
+        self.level = level
+        self.status = status
+        self.alert_type = alert_type
+
+    def check(self, config_mail, addressees):
+        return self.status
+
+    def triggering(self, config_mail, addressees, body=None):
+        self.status = "triggered"
+        if self.vector == "email":
+            subject = "%s alert" % self.alert_type
+            for addressee in addressees:
+                send_mail(config_mail, addressee["mail"], subject, body)
+
+    def resetting(self, config_mail, addressees, body=None):
+        self.status = "iddle"
+        if self.vector == "email":
+            subject = "End of %s alert" % self.alert_type
+            for addressee in addressees:
+                send_mail(config_mail, addressee["mail"], subject, body)
+
+
+class SensorAlert(Alert):
+
+    def __init__(self, sensor, alert_vector=None, level=None, direction=None, trigger=None, reset=None,
+                 status="iddle", json_config=None):
         if json_config is not None:
-            self.vector = json_config["alert-vector"]
-            self.level = json_config["level"]
+            Alert.__init__(self, json_config["alert-vector"], json_config["level"], status, sensor.type)
             self.direction = json_config["direction"]
             self.trigger = json_config["trigger"]
             self.reset = json_config["reset"]
         else:
-            self.vector = alert_vector
-            self.level = level
+            Alert.__init__(self, alert_vector, level, status, sensor.type)
             self.direction = direction
             self.trigger = trigger
             self.reset = reset
         self.sensor = sensor
-        self.status = status
 
     def check(self, config_mail, addressees):
         value = self.sensor.get_value()
@@ -30,28 +55,23 @@ class Alert(object):
         elif self.status == "triggered":
             if self.direction == "over":
                 if value < self.reset:
-                    self.reseting(value, config_mail, addressees)
+                    self.resetting(value, config_mail, addressees)
             elif self.direction == "below":
                 if value > self.reset:
-                    self.reseting(value, config_mail, addressees)
+                    self.resetting(value, config_mail, addressees)
         return self.status
 
-    def triggering(self, value, config_mail, addressees):
-        self.status = "triggered"
+    def triggering(self, config_mail, addressees, body=None):
+        value = self.sensor.get_value()
         sensor_type = self.sensor.type
-        subject = "%s alert" % (sensor_type)
         body = "Module %s, %s sensor : %4.1f %s, expected %s %4.1f" % (
-            self.sensor.module.hwid, sensor_type, value, self.sensor.get_unit(), "<" if self.direction == "over" else ">", self.trigger)
-        if self.vector == "email":
-            for addressee in addressees:
-                send_mail(config_mail, addressee["mail"], subject, body)
+            self.sensor.module.hwid, sensor_type, value, self.sensor.get_unit(),
+            "<" if self.direction == "over" else ">", self.trigger)
+        Alert.triggering(self, config_mail, addressees, body)
 
-    def reseting(self, value, config_mail, addressees):
-        self.status = "iddle"
+    def resetting(self, config_mail, addressees, body=None):
+        value = self.sensor.get_value()
         sensor_type = self.sensor.type
-        subject = "End of %s alert" % (sensor_type)
         body = "Module %s, %s sensor : back to normal (%4.1f %s)" % (
             self.sensor.module.hwid, sensor_type, value, self.sensor.get_unit())
-        if self.vector == "email":
-            for addressee in addressees:
-                send_mail(config_mail, addressee["mail"], subject, body)
+        Alert.resetting(self, config_mail, addressees, body)
