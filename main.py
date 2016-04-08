86930841e9425a0618a45bf91870cd9e150d9645
Adrien J-M. C. Rabian <adrien@rabian.fr>
2016-04-08 15:14:01 +0200

    Adding states persistence.

--- main.py
+++ main.py
@@ -27,15 +27,28 @@ with open('config.json') as config_file:
             for alert in sensor["alerts"]:
                 obj_alert = SensorAlert(obj_sensor, json_config=alert)
                 alerts.append(obj_alert)
+try:
+    states_file_read = open('states.json', 'r')
+except FileNotFoundError:
+    print("States persistence file not found. Initializing with default states.")
+else:
+    states_read = json.load(states_file_read)
+    states_file_read.close()
+    for alert in alerts:
+        if id(alert) in states_read:
+            alert.status = states_read[id(alert)]
 
+states = dict()
 while True:
     print("---- %s ----" % time.asctime(time.localtime(time.time())))
     try:
         for alert in alerts:
-            alert.check(mail_config, addressees)
+            states[id(alert)] = alert.check(mail_config, addressees)
         for sensor in sensors:
             print("Module %s, %s sensor : %4.1f %s" % (sensor.module.hwid, sensor.type, sensor.get_value(), sensor.get_unit()))
     except DisconnectedModuleException as dme:
         print(dme)
     finally:
+        with open("states.json", "w") as states_file_write:
+            states_file_write.write(json.dumps(states))
         YAPI.Sleep(1000)
