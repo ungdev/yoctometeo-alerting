17505f632c71873e39a06f37c95b029be48f819e
Adrien J-M. C. Rabian <adrien@rabian.fr>
2016-05-13 16:06:53 +0200

    Removing useless arguments passing. (reverted from commit e0c5e1af6356feca225d4164136f6f6fc99df3e6)

--- alert.py
+++ alert.py
@@ -1,6 +1,5 @@
 from alerting import *
 from exceptions import *
-from main import addressees, mail_config
 
 
 class Alert(object):
@@ -12,22 +11,22 @@ class Alert(object):
         self.status = status
         self.alert_type = alert_type
 
-    def check(self):
+    def check(self, config_mail, addressees):
         return self.status
 
-    def triggering(self, body=None):
+    def triggering(self, config_mail, addressees, body=None):
         self.status = "triggered"
         if (self.vector == "email") | (self.vector == "*"):
             subject = "%s alert" % self.alert_type
             for addressee in addressees:
-                send_mail(mail_config, addressee["mail"], subject, body)
+                send_mail(config_mail, addressee["mail"], subject, body)
 
-    def resetting(self, body=None):
+    def resetting(self, config_mail, addressees, body=None):
         self.status = "iddle"
         if (self.vector == "email") | (self.vector == "*"):
             subject = "End of %s alert" % self.alert_type
             for addressee in addressees:
-                send_mail(mail_config, addressee["mail"], subject, body)
+                send_mail(config_mail, addressee["mail"], subject, body)
 
 
 class ModuleDisconnectedAlert(Alert):
@@ -36,26 +35,26 @@ class ModuleDisconnectedAlert(Alert):
         Alert.__init__(self, alert_id, alert_vector, level, status, alert_type)
         self.module = module
 
-    def check(self):
+    def check(self, config_mail, addressees):
         try:
             self.module.get_hw_module()
         except DisconnectedModuleException as dme:
             if self.status == "iddle":
-                self.triggering()
+                self.triggering(config_mail, addressees)
         else:
             if self.status == "triggered":
-                self.resetting()
+                self.resetting(config_mail, addressees)
         return self.status
 
-    def triggering(self, body=None):
+    def triggering(self, config_mail, addressees, body=None):
         id = self.module.hwid
         body = "Module %s appears to be disconnected. Alerts related to this module will be suspended pending resolution." % id
-        Alert.triggering(self, body)
+        Alert.triggering(self, config_mail, addressees, body)
 
-    def resetting(self, body=None):
+    def resetting(self, config_mail, addressees, body=None):
         id = self.module.hwid
         body = "Module %s has been reconnected. Resuming normal operation." % id
-        Alert.resetting(self, body)
+        Alert.resetting(self, config_mail, addressees, body)
 
 
 class SensorAlert(Alert):
@@ -74,35 +73,35 @@ class SensorAlert(Alert):
             self.reset = reset
         self.sensor = sensor
 
-    def check(self):
+    def check(self, config_mail, addressees):
         value = self.sensor.get_value()
         if self.status == "iddle":
             if self.direction == "over":
                 if value > self.trigger:
-                    self.triggering()
+                    self.triggering(value, config_mail, addressees)
             elif self.direction == "below":
                 if value < self.trigger:
-                    self.triggering()
+                    self.triggering(value, config_mail, addressees)
         elif self.status == "triggered":
             if self.direction == "over":
                 if value < self.reset:
-                    self.resetting()
+                    self.resetting(value, config_mail, addressees)
             elif self.direction == "below":
                 if value > self.reset:
-                    self.resetting()
+                    self.resetting(value, config_mail, addressees)
         return self.status
 
-    def triggering(self, body=None):
+    def triggering(self, config_mail, addressees, body=None):
         value = self.sensor.get_value()
         sensor_type = self.sensor.type
         body = "Module %s, %s sensor : %4.1f %s, expected %s %4.1f" % (
             self.sensor.module.hwid, sensor_type, value, self.sensor.get_unit(),
             "<" if self.direction == "over" else ">", self.trigger)
-        Alert.triggering(self, body)
+        Alert.triggering(self, config_mail, addressees, body)
 
-    def resetting(self, body=None):
+    def resetting(self, config_mail, addressees, body=None):
         value = self.sensor.get_value()
         sensor_type = self.sensor.type
         body = "Module %s, %s sensor : back to normal (%4.1f %s)" % (
             self.sensor.module.hwid, sensor_type, value, self.sensor.get_unit())
-        Alert.resetting(self, body)
+        Alert.resetting(self, config_mail, addressees, body)
